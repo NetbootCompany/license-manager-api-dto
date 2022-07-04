6a6a45ef9ba4f137169efaf3f6a2791fb1f1ad4b
Thomas ILLIET <contact@thomas-illiet.fr>
2022-07-05 00:39:40 +0200

    Add organization object

---
+++ Src/Enum/OrganizationType.cs
@@ -0,0 +1,22 @@
+﻿// This file is part of LicenceManager. Copyright © 2022 Netboot.
+//
+// LicenceManager is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation, either version 3
+// of the License, or any later version.
+//
+// LicenceManager is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with LicenceManager. If not, see <http://www.gnu.org/licenses/>.
+
+namespace LicenseManager.Api.Dto.Enum;
+
+public enum OrganizationType
+{
+    Personal = 1,
+    Shared = 2,
+}
