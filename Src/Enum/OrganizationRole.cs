9d60d8b109a22c385060124ad39f3aaf81377086
Thomas ILLIET <contact@thomas-illiet.fr>
2022-07-06 22:30:11 +0200

    Add user organization object

---
+++ Src/Enum/OrganizationRole.cs
@@ -0,0 +1,26 @@
+﻿// This file is part of LicenceManager. Copyright © 2022 Netboot.
+//
+// LicenceManager is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation, either version 3
+// of the License, or any later version.
+//
+// LicenceManager is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with LicenceManager. If not, see <http://www.gnu.org/licenses/>.
+
+namespace LicenseManager.Api.Dto.Enum;
+
+/// <summary>
+/// The role of a user in an organization.
+/// </summary>
+public enum OrganizationRole : short
+{
+    Owner = 1,
+    Editor = 2,
+    Viewer = 3
+}
---
+++ Src/UserOrganizationDto.cs
@@ -0,0 +1,29 @@
+﻿// This file is part of LicenceManager. Copyright © 2022 Netboot.
+//
+// LicenceManager is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation, either version 3
+// of the License, or any later version.
+//
+// LicenceManager is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with LicenceManager. If not, see <http://www.gnu.org/licenses/>.
+
+using LicenseManager.Api.Dto.Enum;
+
+namespace LicenseManager.Api.Dto;
+
+/// <summary>
+/// User of organization data transfer object
+/// </summary>
+public class UserOrganizationDto
+{
+    public Guid Id { get; set; }
+    public OrganizationRole Role { get; set; }
+    public UserDto User { get; set; }
+    public DateTimeOffset CreatedUtc { get; set; }
+}
